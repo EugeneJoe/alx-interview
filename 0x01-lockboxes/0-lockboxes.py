e86ee639661e5898e4709c6203fdb5bcc48dce7f
EugeneJoe <jmuthui70@gmail.com>
2021-12-02 20:17:35 +0000

    Add file

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Solution to lockboxes problem
+"""
+
+
+def join(locked, my_keys):
+    """
+    Add obtained keys to our keys list
+    """
+    res = []
+    for key in my_keys:
+        res += locked[key]
+    return res
+
+
+def canUnlockAll(boxes):
+    """
+    Determines whether a series of locked boxes can be opened
+    based on keys that can be attained.
+
+    Solution to the lockboxes problem
+    """
+    index = 0
+    keys = list(set(boxes[0]) | {0})
+    added = True
+    while added:
+        added = False
+        for key in join(boxes, keys[index:]):
+            if key not in keys:
+                keys.append(key)
+                index += 1
+                added = True
+
+    return len(keys) == len(boxes)
