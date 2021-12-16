5fba022c0df9728d1362b074a5e6fa0e01a83469
EugeneJoe <jmuthui70@gmail.com>
2021-12-16 11:10:38 +0000

    Log parser

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+Log parsing
+"""
+import sys
+import re
+
+
+def print_metrics(file_size, status_codes):
+    """
+    Print metrics
+    """
+    print("File size: {}".format(file_size))
+    codes_sorted = sorted(status_codes.keys())
+    for code in codes_sorted:
+        if status_codes[code] > 0:
+            print("{}: {}".format(code, status_codes[code]))
+
+
+possible_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+codes_count = {}
+file_size_total = 0
+count = 0
+if __name__ == "__main__":
+    try:
+        for line in sys.stdin:
+            try:
+                status_code = line.split()[-2]
+                if status_code in (possible_codes and codes_count.keys()):
+                    codes_count[status_code] += 1
+                else:
+                    codes_count[status_code] = 1
+                    # Grab file size
+                file_size = int(line.split()[-1])
+                file_size_total += file_size
+            except:
+                pass
+            # print metrics if 10 lines have been read
+            count += 1
+            if count == 10:
+                print_metrics(file_size_total, codes_count)
+                count = 0
+    except KeyboardInterrupt:
+        print_metrics(file_size_total, codes_count)
+        raise
+    else:
+        print_metrics(file_size_total, codes_count)
